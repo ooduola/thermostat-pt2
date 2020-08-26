4d729d0030611fafdcc319861c604e368c5f254e
Olatunji Oduola <ola.oduola@gmail.com>
2020-08-26 22:02:42 +0100

    Refactor: DisplayWeather now in its own function

--- src/interface.js
+++ src/interface.js
@@ -38,24 +38,39 @@ document.addEventListener("DOMContentLoaded", function () {
 	});
 
 	// Ajax request made using jQuery
-	$.get(
-		"http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=71df5b3a7fbe60b520fbd45ec0c57c74&units=metric",
-		function (data) {
-			$("#city-temperature").text(data.main.temp);
-		}
-	);
+	// $.get(
+	// 	"http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=71df5b3a7fbe60b520fbd45ec0c57c74&units=metric",
+	// 	function (data) {
+	// 		$("#city-temperature").text(data.main.temp);
+	// 	}
+	// );
+	displayWeather("london");
 
 	$("#city-name").submit(function (e) {
 		var city = $("#city").val();
 
-		$.get(
-			"http://api.openweathermap.org/data/2.5/weather?q=" +
-				city +
-				"&appid=71df5b3a7fbe60b520fbd45ec0c57c74&units=metric",
-			function (data) {
-				$("#city-temperature").text(data.main.temp);
-			}
-		);
+		displayWeather(city);
+
+		// $.get(
+		// 	"http://api.openweathermap.org/data/2.5/weather?q=" +
+		// 		city +
+		// 		"&appid=71df5b3a7fbe60b520fbd45ec0c57c74&units=metric",
+		// 	function (data) {
+		// 		$("#city-temperature").text(data.main.temp);
+		// 		$("#current-city").text(data.name);
+		// 	}
+		// );
 		e.preventDefault();
 	});
 });
+
+function displayWeather(city) {
+	var url = "http://api.openweathermap.org/data/2.5/weather?q=";
+	var token = "&appid=71df5b3a7fbe60b520fbd45ec0c57c74";
+	var metric = "&units=metric";
+
+	$.get(url + city + token + metric, function (data) {
+		$("#city-temperature").text(data.main.temp);
+		$("#current-city").text(data.name);
+	});
+}
